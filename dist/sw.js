594bb73c5c7d8fa7c83c1c52205204e74665f78d
Brian Smith <brismuth@gmail.com>
2023-04-07 11:42:20 -0600

    Moving caching model to network first

--- dist/sw.js
+++ dist/sw.js
@@ -18,16 +18,20 @@ self.addEventListener('message', (event) => {
 self.addEventListener("fetch", (e) => {
   e.respondWith(
     (async () => {
-      const r = await caches.match(e.request);
-      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
-      if (r) {
-        return r;
+      try {
+       console.log(`[Service Worker] Attempting live fetch: ${e.request.url}`);
+       const response = await fetch(e.request);
+       const cache = await caches.open(KEY);
+       console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+       cache.put(e.request, response.clone());
+       return response;
+      } catch (err) {
+        console.log(`[Service Worker] Attempting to serve resource from cache: ${e.request.url}`);
+        const r = await caches.match(e.request);
+        if (r) {
+          return r;
+        }
       }
-      const response = await fetch(e.request);
-      const cache = await caches.open(KEY);
-      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
-      cache.put(e.request, response.clone());
-      return response;
     })()
   );
 });
